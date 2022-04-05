e797497feee593ffdfe38f9d50ce88a33a3340ad
Wojciech Kozlowski <w.kozlowski@tudelft.nl>
2022-04-06 01:20:47 +0200

    Update comment in template

--- template/src/eve.py
+++ template/src/eve.py
@@ -6,6 +6,14 @@ class Eve:
         pass
 
     def eavesdrop(self, qubit: Qubit):
+        # IMPLEMENT YOUR EAVESDROPPING CODE HERE
+        #
+        # This method is called for each qubit individually at both ends of the
+        # connection. However, note that these will be actually two separate
+        # instances of Eve - one at each end. Thus, whilst you can share state
+        # from qubit to qubit, this state cannot be shared between the two ends
+        # of the connection.
+        #
         # When measuring make sure to use `inplace=True` when calling
         # `qubit.measure(inpace=True)` so that the qubit is still available when
         # delivered to the application. Otherwise, the qubit is released.
